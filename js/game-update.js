1e4f5a7617599e8d2a2680d2c96a9e9a5e92919e
Christian Haug <chris-haug@gmx.de>
2015-04-19 12:25:09 -0400

    msgContainer fixed and gesture control first version implemented

--- js/game-update.js
+++ js/game-update.js
@@ -6,6 +6,8 @@ function update() {
     //  Reset the players velocity (movement)
     player.body.velocity.x = 0;
 
+
+
     if (cursors.left.isDown)
     {
         //  Move to the left
@@ -28,39 +30,31 @@ function update() {
         player.frame = 4;
     }
 
-    colors.on('track', function(event) {
-    if (event.data.length === 0) {
-      // No colors were detected in this frame.
-    } 
-
-    else {
-      event.data.forEach(function(rect) {
-        if (rect.height > .2 * video.height && rect.width > .2 * video.width)
+    gest.options.subscribeWithCallback(function(gesture) {
+        if (gesture.direction === "Up" || gesture.direction === "Long up")
         {
-            //print location
-            // console.log(rect.x, rect.y, rect.height, rect.width, rect.color); // production
+            //  Jump
+        player.body.velocity.y = -300;
+        }
 
-            //jump
-            if (rect.y  < .2666666 * video.height) {
-                player.body.velocity.y = -350;
-            }
 
-            //move right
-            if (rect.x < .25  * video.width){
-                player.body.velocity.x = 150;
-                player.animations.play('right');
-            }
+        if (gesture.direction === "Left")
+        {
+            //  Move to the left
+        player.body.velocity.x = -2500;
 
-            //move left
-            if (rect.x > .6  * video.width) {
-                player.body.velocity.x = -150;
-                player.animations.play('left');
-            }
-        }	 
-      });
-    }
-    
-  	});
+        player.animations.play('left');
+        }
+
+        if (gesture.direction === "Right")
+        {
+            //  Move to the right
+        player.body.velocity.x = 2500;
+
+        player.animations.play('right');
+        }
+
+    });
 
 
     //  Allow the player to jump if they are touching the ground.
